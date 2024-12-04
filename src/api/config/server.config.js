a7955326d8668451c3742bdd1531a8a71cfae125
michaelvbend <brenzodj@gmail.com>
2024-12-04 17:51:21 +0100

    fix(): update CORS configuration to allow all origins and adjust preflight settings

--- src/api/config/server.config.js
+++ src/api/config/server.config.js
@@ -3,6 +3,8 @@ export const MAX_BODY_SIZE = 5000;
 export const port = 3000;
 
 export const corsOptions = {
-  origin: 'https://portfolio-six-wine-65.vercel.app',
-  optionsSuccessStatus: 200,
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
