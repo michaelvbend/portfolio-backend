c2f0eeb778c8b8c4969a3f6a08a458ee748fb05a
michaelvbend <brenzodj@gmail.com>
2024-12-04 18:37:21 +0100

    fix(): refactor email transport configuration to use environment variables and streamline email sending logic

--- src/api/config/email-transport.config.js
+++ src/api/config/email-transport.config.js
@@ -1,7 +1,6 @@
 export const TransportConfig = {
   host: 'sandbox.smtp.mailtrap.io',
   port: 2525,
-  secure: false,
   auth: {
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASS,
--- src/api/routes/email/email.controller.js
+++ src/api/routes/email/email.controller.js
@@ -1,32 +1,30 @@
 import nodemailer from 'nodemailer';
 import { TransportConfig } from '../../config/email-transport.config.js';
 
-export const createTransporter = () =>
-  nodemailer.createTransport(TransportConfig);
+const transporter = nodemailer.createTransport(TransportConfig);
 
-export async function sendEmail(req, res, transporter = createTransporter()) {
+export async function sendEmail(req, res) {
   const { name, email, company, message } = req.body;
   const isValidEmail = validateEmail(email);
-
   if (!name || !email || !company || !message) {
     return res.status(400).json({ message: 'All fields are required' });
   }
-
   if (!isValidEmail) {
     return res.status(400).json({ message: 'No supported email format.' });
   }
-
   const emailTemplate = generateEmailTemplate(email, name, company, message);
-  handleSendEmail(emailTemplate, res, transporter);
+  handleSendEmail(emailTemplate, res);
 }
 
-function handleSendEmail(emailTemplate, res, transporter) {
+function handleSendEmail(emailTemplate, res) {
   transporter.sendMail(emailTemplate, (error, info) => {
     if (error) {
+      console.log('Error:', error);
       return res.status(500).json({
         message: 'Failed to send email due to server error.',
       });
     }
+    console.log('Email sent: ' + info.response);
     return res.status(200).json({
       status: 'success',
       message: 'Email successfully sent',
