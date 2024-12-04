20a1d3febbcd9f382af7261f91ef80a0937eaa28
michaelvbend <brenzodj@gmail.com>
2024-12-04 18:46:21 +0100

    fix(): update CORS configuration to allow specified domains for requests

--- src/api/config/server.config.js
+++ src/api/config/server.config.js
@@ -3,7 +3,7 @@ export const MAX_BODY_SIZE = 5000;
 export const port = 3000;
 
 export const corsOptions = {
-  origin: 'https://portfolio-six-wine-65.vercel.app',
+  origin: ['https://michaelvdbend.dev', 'https://www.michaelvdbend.dev'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204,
