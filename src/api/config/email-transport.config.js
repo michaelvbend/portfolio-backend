08994e512310a37b909124c478b4e3252ce8751d
michaelvbend <brenzodj@gmail.com>
2024-12-04 18:06:12 +0100

    fix(): update email transport configuration to use environment variables for authentication

--- src/api/config/email-transport.config.js
+++ src/api/config/email-transport.config.js
@@ -1,9 +1,9 @@
 export const TransportConfig = {
   host: 'sandbox.smtp.mailtrap.io',
   port: 2525,
-  secure: false, // use false for STARTTLS; true for SSL on port 465
+  secure: false,
   auth: {
-    user: '7edf96b7edfea7',
-    pass: '50882f0f6e882a',
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASS,
   },
 };
