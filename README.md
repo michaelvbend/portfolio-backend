0cde7b3b4f799ebeaf0f6ededb83e8d7cb3bf95c
michaelvbend <brenzodj@gmail.com>
2024-12-05 18:08:21 +0100

    docs: update README with installation instructions and environment configuration
    refactor: modify email controller to create transporter function and improve error logging

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# portfolio-backend
+# Portfolio Backend
+
+This is the backend for the portfolio website of Michael van der Bend. It is built using Express.js and provides APIs for handling various functionalities such as sending emails.
+
+## Installation
+
+1. Clone the repository:
+
+   ```sh
+   git clone https://github.com/michaelvbend/portfolio-backend.git
+   cd portfolio-backend
+
+   ```
+
+2. Install the dependencies:
+
+   ```sh
+   npm install
+
+   ```
+
+3. Configuration
+   Create a .env file in the root directory and add the following environment variables:
+
+   ```sh
+   MONGODB_URI=your_mongodb_uri
+   PORT=3000
+   ```
+
+   Update the src/config/email-transport.config.js file with your email service configuration:
+
+   ```sh
+   export const TransportConfig = {
+   host: 'smtp.example.com',
+   port: 587,
+   secure: false, // true for 465, false for other ports
+   auth: {
+    user: 'your-email@example.com',
+    pass: 'your-email-password',
+   },
+   };
+   ```
+
+## Running the application
+
+```sh
+npm run dev
+npm test
+```
--- src/api/routes/email/email.controller.js
+++ src/api/routes/email/email.controller.js
@@ -1,30 +1,33 @@
 import nodemailer from 'nodemailer';
 import { TransportConfig } from '../../config/email-transport.config.js';
 
-const transporter = nodemailer.createTransport(TransportConfig);
+export const createTransporter = () =>
+  nodemailer.createTransport(TransportConfig);
 
-export async function sendEmail(req, res, customTransporter = transporter) {
+export async function sendEmail(req, res, transporter = createTransporter()) {
   const { name, email, company, message } = req.body;
   const isValidEmail = validateEmail(email);
+
   if (!name || !email || !company || !message) {
     return res.status(400).json({ message: 'All fields are required' });
   }
+
   if (!isValidEmail) {
     return res.status(400).json({ message: 'No supported email format.' });
   }
+
   const emailTemplate = generateEmailTemplate(email, name, company, message);
-  handleSendEmail(emailTemplate, res, customTransporter);
+  handleSendEmail(emailTemplate, res, transporter);
 }
 
-function handleSendEmail(emailTemplate, res, customTransporter) {
-  customTransporter.sendMail(emailTemplate, (error, info) => {
+function handleSendEmail(emailTemplate, res, transporter) {
+  transporter.sendMail(emailTemplate, (error, info) => {
     if (error) {
-      console.log('Error:', error);
+      console.error('Error sending email:', error);
       return res.status(500).json({
         message: 'Failed to send email due to server error.',
       });
     }
-    console.log('Email sent: ' + info.response);
     return res.status(200).json({
       status: 'success',
       message: 'Email successfully sent',
@@ -42,6 +45,6 @@ function generateEmailTemplate(email, name, company, message) {
     from: email,
     to: 'mvanderbend@gmail.com',
     subject: 'Portfolio contact form submission',
-    text: `From: ${name}\nIn: ${company}\nmessage: ${message}`,
+    text: `From: ${name}\nIn: ${company}\nMessage: ${message}`,
   };
 }
